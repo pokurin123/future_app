6d69eccdc9f163ca76b5dea5fa810e48fc332d7c
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2021-01-19 03:31:44 +0900

    掲載物関係SQL

---
+++ SQL/pre_publication.sql
@@ -0,0 +1,28 @@
+CREATE TABLE pre_publication(
+   publication_id    INTEGER  NOT NULL PRIMARY KEY 
+  ,publication_name  VARCHAR(50) NOT NULL
+  ,publication_image VARCHAR(50) NOT NULL
+  ,sc_number         INTEGER  NOT NULL
+  ,tags              VARCHAR(15) NOT NULL
+  ,pdf_pass          VARCHAR(50) NOT NULL
+);
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (1,'VRマップの自動生成','3Dマップ自動生成.png',1922074,'VR','./static/3Dマップ自動生成');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (2,'研究・活動内容掲載プラットフォーム','sea_2.png',1922074,'Web','./static/sea_2.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (3,'最適経路の算出','sea_3.png',1922074,'AI','./static/sea_3.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (4,'交通機関のルート検索を一括化','sea_4.png',1922074,'AI','./static/sea_4.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (5,'人工知能における人間の役割','sea_5.png',1922074,'AI','./static/sea_5.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (6,'インフルエンザとワクチンの効能について','sea_6.png',1922074,'Web','./static/sea_6.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (7,'マーケティングにおけるデータサイエンスとは','sea_7.png',1922074,'iOS','./static/sea_7.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (8,'VR空間の自動生成研究','sea_8.png',1922074,'XR','./static/sea_8.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (9,'リレーショナルDBの構築、考察','sea_9.png',1922074,'Web','./static/sea_9.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (10,'2020年オープンキャンパス展示作品','sea_10.png',1922074,'VR','./static/sea_10.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (11,'授業内容検索アプリ開発','sea_11.png',1922074,'Web','./static/sea_11.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (12,'機械学習利用のアイデア提案','sea_12.png',1922074,'AI','./static/sea_12.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (13,'db_test','sea_13.png',1922074,'Web','./static/sea_13.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (14,'db_test','sea_13.png',1922074,'Web','./static/sea_13.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (15,'db_test','sea_13.png',1922074,'iOS','./static/sea_13.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (16,'db_test','sea_13.png',1922074,'Unity','./static/sea_13.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (17,'db_test','sea_13.png',1922074,'Unity','./static/sea_13.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (18,'db_test','sea_13.png',1922074,'VR','./static/sea_13.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (19,'db_test','sea_13.png',1922074,'XR','./static/sea_13.pdf');
+INSERT INTO pre_publication(publication_id,publication_name,publication_image,sc_number,tags,pdf_pass) VALUES (20,'db_test','sea_13.png',1922074,'XR','./static/sea_13.pdf');
