43ac8828420468e9787e5e4417cfae0c3a12c137
Kimura Taiki <60126875+pokurin123@users.noreply.github.com>
2021-01-19 03:37:43 +0900

    ログインテスト用SQL

---
+++ SQL/test_login.sql
@@ -0,0 +1,13 @@
+CREATE TABLE test_login(
+   id        INTEGER  NOT NULL PRIMARY KEY 
+  ,name      VARCHAR(10) NOT NULL
+  ,sc_number INTEGER  NOT NULL
+);
+INSERT INTO test_login(id,name,sc_number) VALUES (1,'木村太紀',1922074);
+INSERT INTO test_login(id,name,sc_number) VALUES (2,'テスト太郎',12345);
+INSERT INTO test_login(id,name,sc_number) VALUES (3,'テスト花子',678910);
+INSERT INTO test_login(id,name,sc_number) VALUES (4,'テストよしお',11121314);
+INSERT INTO test_login(id,name,sc_number) VALUES (5,'テストドン',15161718);
+INSERT INTO test_login(id,name,sc_number) VALUES (6,'テストン',19202122);
+INSERT INTO test_login(id,name,sc_number) VALUES (7,'テスタロッサ',23242526);
+INSERT INTO test_login(id,name,sc_number) VALUES (8,'訪問者',0);
